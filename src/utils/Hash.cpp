23b66a9ab4e702ef10a52483d04be0337c6b219b
Moria Pedhazur <moria313@gmail.com>
2025-04-23 18:27:47 +0300

    Add arraySize to Hash constructor in header and cpp

--- src/utils/Hash.cpp
+++ src/utils/Hash.cpp
@@ -1,6 +1,6 @@
 #include "Hash.h"
 
-Hash::Hash(std::unique_ptr<int[]> hashArray) : hashArray(std::move(hashArray)) {}
+Hash::Hash(std::unique_ptr<int[]> hashArray, int arraySize): hashArray(std::move(hashArray)), arraySize(arraySize) {}
 
 std::vector<int> Hash::execute(const Url& url) const {
     
--- src/utils/Hash.h
+++ src/utils/Hash.h
@@ -8,8 +8,9 @@ class Url;
 class Hash {
 private:
     std::unique_ptr<int[]> hashArray;
+    int arraySize;
 public:
-    Hash(std::unique_ptr<int[]> hashArray);
+    Hash(std::unique_ptr<int[]> hashArrayת int arraySize);
     std::vector<int> execute(const Url& url) const;
 };
 
