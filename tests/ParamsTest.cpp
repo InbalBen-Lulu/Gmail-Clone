f71cfe3508332e786056dd088f5c3a01a987c308
InbalBen-Lulu <inbal014789@gmail.com>
2025-04-24 15:51:09 +0300

    Update tests across multiple classes

--- tests/ParamsTest.cpp
+++ tests/ParamsTest.cpp
@@ -8,18 +8,18 @@ using namespace std;
 TEST(ParamsTest, NoExistingFile_NewFileShouldBeTrue) {
     remove("../data/params.txt"); 
 
-    int array[2] = {8, 2};
+    std::vector<int> array = {8, 2};
     Params params(8, array);
 
     EXPECT_TRUE(params.getNewFile());
 }
 
 TEST(ParamsTest, FileExists_NewFileShouldBeFalse) {
-    int array1[2] = {8, 2};
+    std::vector<int> array1 = {8, 2};
     Params firstParams(8, array1);
 
     // Same config
-    int array2[2] = {8, 2}; 
+    std::vector<int> array2 = {8, 2}; 
     Params secondParams(8, array2);
 
     EXPECT_FALSE(secondParams.getNewFile());
@@ -28,10 +28,10 @@ TEST(ParamsTest, FileExists_NewFileShouldBeFalse) {
 TEST(ParamsTest, InitCreatesFileIfNotExists) {
     remove("../data/params.txt"); // Delete if exists
 
-    int configArray[2] = {8, 2};
+    std::vector<int> configArray = {8, 2};
     Params params(8, configArray);
 
     ifstream file("../data/params.txt");
     EXPECT_TRUE(file.is_open()); // File should now exist
-    file.close();
+    file.close();
 }
