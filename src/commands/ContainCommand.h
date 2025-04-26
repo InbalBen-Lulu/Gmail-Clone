87a71f2fb8184c900050b9c1e3830ccbb64ead98
Moria Pedhazur <moria313@gmail.com>
2025-04-26 12:34:16 +0300

    PROJ-15 Add documentation comments to command interfaces and classes

--- src/commands/ContainCommand.h
+++ src/commands/ContainCommand.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "ICommand.h"
-// #include "../data/BloomFilter.h"
-// #include "../data/BlackList.h"
-// #include "../utils/Hash.h"
-// #include "../utils/Url.h"
 
+// Command that checks if a given URL exists in the Bloom filter and the blacklist
 class ContainCommand : public ICommand {
 private:
     BloomFilter& bloomFilter;
     BlackList& blackList;
 public:
+    // Constructor: initialize with references to Bloom filter and blacklist
     ContainCommand(BloomFilter& bloom, BlackList& bl);
+
+    /* Execute the containment command: 
+     first check the Bloom filter, then verify in the blacklist if necessary  */
     void execute(const Url& url, Hash& hash, IIOHandler& io) override;
 };
--- src/commands/ICommand.h
+++ src/commands/ICommand.h
@@ -6,8 +6,12 @@
 #include "../utils/Url.h"
 #include "../io/IIOHandler.h"
 
+// Interface for command classes that operate on URLs using hash functions and I/O
 class ICommand {
 public:
+    // Execute the command with the given URL, hash function, and I/O handler
     virtual void execute(const Url& url, Hash& hash, IIOHandler& io) = 0;
+    
+     // Virtual destructor to ensure proper cleanup in derived classes
     virtual ~ICommand() = default;
 };
