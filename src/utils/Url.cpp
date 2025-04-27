f3d709c24b5a8f5d341f02b696ce46397f006437
Moria Pedhazur <moria313@gmail.com>
2025-04-27 14:34:20 +0300

    PROJ-20 delete isValid func from Url

--- src/utils/Url.cpp
+++ src/utils/Url.cpp
@@ -4,25 +4,13 @@
 // Constructor: Initializes a Url instance with the given URL string.
 Url::Url(const std::string& url) : urlPath(url) {}
 
-/*
- * Checks if the stored URL matches a standard URL pattern.
- * Returns true if valid, false otherwise.
- */
-bool Url::isValid() const {
-    static const std::regex urlRegex(
-        R"(^((https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,})(\/\S*)?$)",
-        std::regex::icase
-    );
-    return std::regex_match(urlPath, urlRegex);
-}
-
 // Returns the stored URL path as a string.
 std::string Url::getUrlPath() const {
     return urlPath;
 }
 
 /*
- * Less-than operator overload to allow Url objects to be used in ordered containers (e.g., std::set).
+ * Less-than operator overload to allow Url objects to be used in ordered containers.
  * Compares based on the urlPath string.
  */
 bool Url::operator<(const Url& other) const {
