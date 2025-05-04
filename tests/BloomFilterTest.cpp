6165cd1516110e09dbb4998f5872f6ef932cb10b
InbalBen-Lulu <inbal014789@gmail.com>
2025-05-04 18:18:18 +0300

    test(BloomFilter): verify persistence of added hash bits across instances

--- tests/BloomFilterTest.cpp
+++ tests/BloomFilterTest.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 TEST(BloomFilterTest, AddedHashResultsAreSet) {
+    remove("bloom.txt");  // ensure clean start
+
     BloomStorage storage(true);
     BloomFilter filter(storage, 8);
+
     vector<int> hashResults = {0, 1, 0, 1, 1, 0, 0, 0};
     filter.add(hashResults);
 
+    // Check with original filter
     EXPECT_TRUE(filter.contain(hashResults));
+
+    // Now reload with new instance (persistence test)
+    BloomStorage storage2(false);
+    BloomFilter filter2(storage2, 8);
+    EXPECT_TRUE(filter2.contain(hashResults));
 }
 
 TEST(BloomFilterTest, EmptyFilterReturnsFalse) {
