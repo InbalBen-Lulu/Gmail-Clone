4bfdf3a516b80a8399363de3521486766b45eeba
InbalBen-Lulu <inbal014789@gmail.com>
2025-05-05 01:08:55 +0300

    Update comments in ICommand interface for clarity and accuracy

--- src/commands/ICommand.h
+++ src/commands/ICommand.h
@@ -5,10 +5,10 @@
 #include "../utils/Hash.h"
 #include "../utils/Url.h"
 
-// Interface for command classes that operate on URLs using hash functions and I/O
+// Interface for command classes that operate on URLs using hash functions
 class ICommand {
 public:
-    // Execute the command with the given URL, hash function, and I/O handler
+    // Execute the command with the given URL and hash function
     virtual std::string execute(const Url& url, Hash& hash) = 0;
     
      // Virtual destructor to ensure proper cleanup in derived classes
