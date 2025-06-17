44b0ebc33f3c204022ec9f0bd748b35a12f1d2fb
Moria Pedhazur <moria313@gmail.com>
2025-06-17 16:08:56 +0300

    implement main app routes

--- mail_client/src/App.js
+++ mail_client/src/App.js
@@ -1,23 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import DemoForm from './pages/DemoForm';
+import DemoSignup from './pages/SignupPage';
+import DemoSignin from './pages/SigninPage';
 
-import './App.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/signin" element={<DemoSignin />} />
+            <Route path="/signup" element={<DemoSignup />} />
+            <Route path="*" element={<Navigate to="/signin" replace />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
