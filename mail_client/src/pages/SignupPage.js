f1abbb33346dfc0442ac98543f7b503d6855d755
Moria Pedhazur <moria313@gmail.com>
2025-06-17 16:11:42 +0300

    PROJ-226 craete signup page

---
+++ mail_client/src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import SignupForm from '../components/signup/SignupForm';
+
+const DemoSignin = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default DemoSignin;
