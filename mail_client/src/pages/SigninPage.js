07e6d8156906357a54ff8719914859e495abc2dd
Moria Pedhazur <moria313@gmail.com>
2025-06-17 16:11:11 +0300

    PROJ-225 create login page

---
+++ mail_client/src/pages/SigninPage.js
@@ -0,0 +1,11 @@
+import SigninForm from '../components/signin/SigninForm';
+
+const DemoSignin = () => {
+  return (
+    <div>
+      <SigninForm />
+    </div>
+  );
+};
+
+export default DemoSignin;
