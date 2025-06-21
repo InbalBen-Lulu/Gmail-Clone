a2e613b4f5509da3e3b95431bda1e9d0d6a67358
Moria Pedhazur <moria313@gmail.com>
2025-06-21 16:26:46 +0300

    add ToggleThemeButton to SigninPage for theme switching

--- mail_client/src/pages/SigninPage.js
+++ mail_client/src/pages/SigninPage.js
@@ -1,4 +1,5 @@
 import SigninForm from '../components/signin/SigninForm';
+import ToggleThemeButton from '../components/common/ToggleThemeButton';
 
 /**
  * SigninPage is the main page component for handling user sign-in.
@@ -7,7 +8,12 @@ import SigninForm from '../components/signin/SigninForm';
 const SigninPage = () => {
   return (
     <div>
-      <SigninForm />
+      <div style={{ position: 'relative', minHeight: '100vh' }}>
+        <div style={{ position: 'absolute', top: '1rem', right: '1rem' }}>
+          <ToggleThemeButton />
+        </div>
+        <SigninForm />
+      </div>
     </div>
   );
 };
--- mail_client/src/pages/SignupPage.js
+++ mail_client/src/pages/SignupPage.js
@@ -1,5 +1,5 @@
 import SignupForm from '../components/signup/SignupForm';
-
+import ToggleThemeButton from '../components/common/ToggleThemeButton';
 
 /**
  * SignupPage is the main page component for user registration.
@@ -8,7 +8,12 @@ import SignupForm from '../components/signup/SignupForm';
 const SignupPage = () => {
   return (
     <div>
-      <SignupForm />
+      <div style={{ position: 'relative', minHeight: '100vh' }}>
+        <div style={{ position: 'absolute', top: '1rem', right: '1rem' }}>
+          <ToggleThemeButton />
+        </div>
+        <SignupForm />
+      </div>
     </div>
   );
 };
