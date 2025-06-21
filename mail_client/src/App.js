bee9d01c447d9fdb73fa0934d7b306ee89400f71
Moria Pedhazur <moria313@gmail.com>
2025-06-21 16:14:18 +0300

    setup app routes and context providers

--- mail_client/src/App.js
+++ mail_client/src/App.js
@@ -1,22 +1,44 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
+import MailPage from './pages/MailPage';
+import PrivateRoute from './components/common/PrivateRoute';
 
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 
-
 function App() {
   return (
+    // Wrap entire app with authentication and theme context providers
     <AuthProvider>
       <ThemeProvider>
-        <Router>
-          <Routes>
-            <Route path="/signin" element={<SigninPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="*" element={<Navigate to="/signin" replace />} />
-          </Routes>
-        </Router>
+          <Router>
+            <Routes>
+              {/* Public routes for authentication */}
+              <Route path="/signin" element={<SigninPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+
+              {/* Protected route for mail category view */}
+              <Route path="/mails/:category" element={
+                <PrivateRoute>
+                  <MailPage />
+                </PrivateRoute>
+              } />
+
+              {/* Protected route for specific mail view */}
+              <Route path="/mails/:category/:mailId" element={
+                <PrivateRoute>
+                  <MailPage />
+                </PrivateRoute>
+              } />
+
+              {/* Redirect root path to signin */}
+              <Route path="/" element={<Navigate to="/signin" replace />} />
+
+              {/* Catch-all route redirects unknown paths to signin */}
+              <Route path="*" element={<Navigate to="/signin" replace />} />
+            </Routes>
+          </Router>
       </ThemeProvider>
     </AuthProvider>
   );
